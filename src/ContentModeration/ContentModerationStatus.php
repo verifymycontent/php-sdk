f686baec58adba54b40f9878d4725471cd468f81
Jorge Junior <jorge.junior.568@live.com>
2022-11-24 13:40:15 -0300

    feat: ContentModerationStatus

--- src/ContentModeration/ContentModerationStatus.php
+++ src/ContentModeration/ContentModerationStatus.php
@@ -4,10 +4,19 @@ namespace VerifyMyContent\SDK\ContentModeration;
 
 interface ContentModerationStatus
 {
-    const APPROVED = 'Approved';
-    const REJECTED = 'Rejected';
-    const FAILED = 'Failed';
-    const AWAITING_PEOPLE = 'Awaiting People';
-    const AWAITING_AUTOMATION = 'Awaiting Automation';
-    const AWAITING_MODERATION = 'Awaiting Moderation';
+    const STATIC_APPROVED = 'Approved';
+    const STATIC_REJECTED = 'Rejected';
+    const STATIC_FAILED = 'Failed';
+    const STATIC_AWAITING_PEOPLE = 'Awaiting People';
+    const STATIC_AWAITING_AUTOMATION = 'Awaiting Automation';
+    const STATIC_AWAITING_MODERATION = 'Awaiting Moderation';
+
+    const LIVE_WAITING = 'Waiting';
+    const LIVE_AUTHORIZED = 'Authorized';
+    const LIVE_DENIED = 'Denied';
+    const LIVE_STARTED = 'Started';
+    const LIVE_FINISHED = 'Finished';
+    const LIVE_STOP_REQUESTED = 'Stop Requested';
+    const LIVE_HALTED = 'Halted';
+    const LIVE_FAILED = 'Failed';
 }
