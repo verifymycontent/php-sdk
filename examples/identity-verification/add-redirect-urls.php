5e9928b73bea0f55ec67469b03c54af2c161b68e
Bruno <brunodsosa@gmail.com>
2023-07-25 16:27:04 -0300

    redirect urls examples

---
+++ examples/identity-verification/add-redirect-urls.php
@@ -0,0 +1,22 @@
+<?php
+
+use VerifyMyContent\SDK\VerifyMyContent;
+
+require_once __DIR__ . "/../../vendor/autoload.php";
+
+# Getting env variables
+$API_KEY = getenv("API_KEY");
+$API_SECRET = getenv("API_SECRET");
+
+# Setup SDK
+$vmc = new VerifyMyContent($API_KEY, $API_SECRET);
+$vmc->useSandbox();
+
+try{
+    $vmc->addRedirectUrls(["https://teste1.com", "https://teste2.com"]);
+    echo "done!";
+}catch(Exception $e){
+    echo "error";
+    var_export($e);
+}
+
